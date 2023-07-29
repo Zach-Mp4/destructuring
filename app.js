3cda2a1fe4d57697503c71353f0b8dcfffdeee43
Zachary Buchanan <zach@Zacharys-MBP.attlocal.net>
2023-07-28 19:20:17 -0500

    finished exercise

---
+++ app.js
@@ -0,0 +1,21 @@
+// Question 1: 8, 1846
+// Question 2: {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+// Quesion 3: Your name is Alejandro and you like purple, Your name is Melissa and you like green, Your name is undefined and you like green
+
+// Question 1: Maya, Marissa, Chi
+// Question 2: "Raindrops on roses", "whiskers on kittens", ["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+// Question 3: [10,30,20]
+
+const obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  
+  const {a,b} = obj.numbers
+
+  [arr[0], arr[1]] = [arr[1], arr[0]];
+
+  const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest});
+
